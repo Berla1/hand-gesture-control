6cdbe96c1662727e0131a08c81aa605d8c24bd13
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-10-02 18:46:30 -0300

    dedao

--- mqtt.py
+++ mqtt.py
@@ -19,20 +19,24 @@ def send_servo_angle(angle):
     client.publish(mqtt_topic, str(angle))  
 
 # Funções para identificar se os dedos estão levantados
-def is_index_finger_up(hand_landmarks):
-    return hand_landmarks[8].y < hand_landmarks[6].y  # Índice
+def is_index_finger_down(hand_landmarks):
+    return hand_landmarks[8].y > hand_landmarks[6].y  # Índice
 
-def is_middle_finger_up(hand_landmarks):
-    return hand_landmarks[12].y < hand_landmarks[10].y  # Médio
+def is_middle_finger_down(hand_landmarks):
+    return hand_landmarks[12].y > hand_landmarks[10].y  # Médio
 
-def is_pinky_finger_up(hand_landmarks):
-    return hand_landmarks[20].y < hand_landmarks[18].y  # Mindinho
+def is_pinky_finger_down(hand_landmarks):
+    return hand_landmarks[20].y > hand_landmarks[18].y  # Mindinho
+
+def is_thumb_finger_down(hand_landmarks):
+    return hand_landmarks[4].x > hand_landmarks[2].x  # Dedão
+
 
 # Inicializa a webcam
 cap = cv2.VideoCapture(0)
 
 # Inicializa MediaPipe para detectar mãos
-with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7) as hands:
+with mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7, min_tracking_confidence=0.7) as hands:
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -47,17 +51,21 @@ with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_
         # Verifica se há mãos detectadas
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                if is_index_finger_up(hand_landmarks.landmark):
+                if is_index_finger_down(hand_landmarks.landmark):
                     print("Indicador levantado! Enviando valor 45 via MQTT...")
                     send_servo_angle(45)
 
-                elif is_middle_finger_up(hand_landmarks.landmark):
+                elif is_middle_finger_down(hand_landmarks.landmark):
                     print("Dedo médio levantado! Enviando valor 180 via MQTT...")
                     send_servo_angle(180)
 
-                elif is_pinky_finger_up(hand_landmarks.landmark):
+                elif is_pinky_finger_down(hand_landmarks.landmark):
                     print("Mindinho levantado! Enviando valor 0 via MQTT...")
                     send_servo_angle(0)
+
+                elif is_thumb_finger_down(hand_landmarks.landmark):
+                    print("Dedão levantado levantado! Enviando valor 90 via MQTT...")
+                    send_servo_angle(90)
 
                 # Desenha os pontos da mão
                 mp_drawing.draw_landmarks(
