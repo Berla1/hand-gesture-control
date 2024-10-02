bdca78fb5efb3a021044638fbc555585f91b7796
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-10-02 19:12:12 -0300

    dedo indicador e dedao juntos

--- mqtt.py
+++ mqtt.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import mediapipe as mp
 import paho.mqtt.client as mqtt
 
@@ -14,13 +15,22 @@ client.connect(broker_address)
 # Tópico MQTT para publicação
 mqtt_topic = "/TEF/device010/servo/cmd"  
 
+def calculate_distance(point1, point2):
+    return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
+
+def is_thumb_and_index_touching(hand_landmarks):
+    thumb_tip = hand_landmarks[4]  
+    index_tip = hand_landmarks[8]  
+    distance = calculate_distance(thumb_tip, index_tip)
+    return distance < 0.05  
+
 # Função para enviar o ângulo do servo via MQTT
 def send_servo_angle(angle):
     client.publish(mqtt_topic, str(angle))  
 
 # Funções para identificar se os dedos estão levantados
 def is_index_finger_down(hand_landmarks):
-    return hand_landmarks[8].y > hand_landmarks[6].y  # Índice
+    return hand_landmarks[8].y > hand_landmarks[6].y  # Indicador
 
 def is_middle_finger_down(hand_landmarks):
     return hand_landmarks[12].y > hand_landmarks[10].y  # Médio
@@ -30,7 +40,6 @@ def is_pinky_finger_down(hand_landmarks):
 
 def is_thumb_finger_down(hand_landmarks):
     return hand_landmarks[4].x > hand_landmarks[2].x  # Dedão
-
 
 # Inicializa a webcam
 cap = cv2.VideoCapture(0)
@@ -66,6 +75,15 @@ with mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7, min_tracking_
                 elif is_thumb_finger_down(hand_landmarks.landmark):
                     print("Dedão levantado levantado! Enviando valor 90 via MQTT...")
                     send_servo_angle(90)
+                
+                if is_thumb_and_index_touching(hand_landmarks.landmark):
+                   cv2.putText(frame, "Polegar e Indicador juntos!", 
+                                (50, 50),  # Posição (x, y) no vídeo
+                                cv2.FONT_HERSHEY_SIMPLEX,  
+                                1,  # Tamanho da fonte
+                                (0, 0, 255),  # Cor (BGR): verde
+                                2,  # Espessura da linha
+                                cv2.LINE_AA)  # Tipo de linha
 
                 # Desenha os pontos da mão
                 mp_drawing.draw_landmarks(
@@ -78,7 +96,7 @@ with mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7, min_tracking_
 
         # Exibe o vídeo na tela
         cv2.imshow("Webcam - Detecção de Mãos", frame)
-
+        # Fechar janela da webcam
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
